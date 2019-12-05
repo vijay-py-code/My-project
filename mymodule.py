2d214669f65226f3f3b97f924715f5d76c95de57
vijay-py-code <58543474+vijay-py-code@users.noreply.github.com>
2019-12-05 14:17:30 +0800

    postal code part added
    postal code part added

--- mymodule.py
+++ mymodule.py
@@ -7,3 +7,17 @@ import json
 
 def get_weather(city):
     print ("Hello to GitHub")
+
+def postal_lookup(postal_code):
+    sock = urlopen("http://api.postcodes.io/postcodes/" + postal_code)
+    result = sock.read()                            
+    sock.close()                                        
+    details = json.loads(result)
+    return (details["result"]["latitude"], details["result"]["longitude"])
+
+if __name__ == "__main__":
+    degrees = get_weather("SINGAPORE")
+    print("Weather in SINGAPORE is %.2f degrees Celsius" % degrees)
+
+    location = postal_lookup("B323PP")
+    print(location)
